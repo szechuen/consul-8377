c80bbb4545c5c46a818cab9f89d3391c41158953
Sze Chuen Tan <szechuen@cloudflare.com>
2020-07-28 08:09:11 -0700

    Lengthen sleeps and print raft stats after restarts

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 echo "Waiting for cluster to be ready..."
 while [ $(curl -s http://127.0.0.1:8501/v1/status/leader) = '""' ]; do :; done
+sleep 2
 echo ""
 
 echo "Creating health check..."
@@ -24,9 +25,15 @@ echo "server1"; curl -s http://127.0.0.1:8501/v1/snapshot?stale > /dev/null
 echo ""
 
 echo "Leaving and restarting servers..."
-echo "server3"; curl --request PUT http://127.0.0.1:8503/v1/agent/leave; sleep 5
-echo "server2"; curl --request PUT http://127.0.0.1:8502/v1/agent/leave; sleep 5
-echo "server1"; curl --request PUT http://127.0.0.1:8501/v1/agent/leave; sleep 5
+echo "server3"; curl --request PUT http://127.0.0.1:8503/v1/agent/leave; sleep 10
+echo "server2"; curl --request PUT http://127.0.0.1:8502/v1/agent/leave; sleep 10
+echo "server1"; curl --request PUT http://127.0.0.1:8501/v1/agent/leave; sleep 10
+echo ""
+
+echo "Checking Raft stats after restarts..."
+echo "server1: "; curl -s http://127.0.0.1:8501/v1/agent/self | jq -cM '.Stats.raft'
+echo "server2: "; curl -s http://127.0.0.1:8502/v1/agent/self | jq -cM '.Stats.raft'
+echo "server3: "; curl -s http://127.0.0.1:8503/v1/agent/self | jq -cM '.Stats.raft'
 echo ""
 
 echo "Querying health check -- this should now be INconsistent across servers..."
@@ -36,7 +43,7 @@ echo "server3: "; curl -w '\n' http://127.0.0.1:8503/v1/health/checks/service1?s
 echo ""
 
 echo "Performing CAS check update..."
-curl -s http://127.0.0.1:8501/v1/health/checks/service1?consistent | jq -r '.[0] | .Status = "critical" | [{"Check": {"Verb": "cas", "Check": .}}]' | curl -w '\n' --request PUT --data @- http://127.0.0.1:8501/v1/txn
+curl -s http://127.0.0.1:8501/v1/health/checks/service1?consistent | jq '.[0] | .Status = "critical" | [{"Check": {"Verb": "cas", "Check": .}}]' | curl -w '\n' --request PUT --data @- http://127.0.0.1:8501/v1/txn
 sleep 2
 echo ""
 
